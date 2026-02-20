8833f479880a5dfa79bfc3d1cf9139840298eb8a
Xylian <xylian.cocquyt@student.hogent.be>
2026-02-20 16:22:09 +0100

    Added some more code, added a way for objects to be gameobjects like in editors. This way the overworld code could be refactored.

--- scripts/Overworld.js
+++ scripts/Overworld.js
@@ -7,6 +7,34 @@ class Overworld {
 
 
     init() {
-        console.log("Hello from the Overworld", this);
+        // Code for drawing background / maybe tilemap later
+        const backgroundSprite = new Image();
+        backgroundSprite.onload = () => {
+            this.ctx.drawImage(backgroundSprite, 0, 0)
+        };
+        backgroundSprite.src = "../images/maps/DemoLower.png";
+
+        // Place some game objects
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+
+        })
+
+        const npc1 = new GameObject({
+            x: 7,
+            y: 8,
+            src: "/images/characters/people/npc1.png"
+            
+        })
+
+
+
+        // Draw the game objects on the canvas.
+        setTimeout(() => { // will be done in loop eventually
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 50)
+        
     }
 }
---
+++ scripts/Sprite.js
@@ -0,0 +1,56 @@
+class Sprite {
+    constructor(config) {
+
+        //Set up the image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;
+        }
+
+        // Shadow
+        this.shadow = new Image();
+        this.useShadow = true;
+        if (this.useShadow) {
+            this.shadow.src = "/images/characters/shadow.png";
+        }
+
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+        
+
+
+        // Configure Animation & initial State
+
+        this.animations = config.animations || {
+            idleDown: [
+                [0, 0]
+            ],
+        }    
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
+          
+        // Reference to the gameobject
+        this.gameObject = config.gameObject;
+    
+    }
+
+
+        draw(ctx) {
+            const x = this.gameObject.x * 16 - 8;
+            const y = this.gameObject.y * 16 - 18;
+
+            this.isShadowLoaded && ctx.drawImage(this.shadow,
+                x, y
+            )
+            
+            this.isLoaded && ctx.drawImage(this.image, 
+                0, 0,       // Startpoint top crop
+                32, 32,     // width of pixel art
+                x, y,       // sprite position
+                32, 32      // sprite size
+            )
+
+        }
+}
